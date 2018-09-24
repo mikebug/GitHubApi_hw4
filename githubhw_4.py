638dd1f787f5f6309fecbbe70d0ad86607eb9a27
mikebug <mbuglion@stevens.edu>
2018-09-23 22:01:39 -0400

    First Commit
    First commit

---
+++ githubhw_4.py
@@ -0,0 +1,29 @@
+#Mike Buglione I plegde my honor that I have abided by the stevens honor system
+
+import requests
+import json
+
+
+input = input("GitHub profile name: ")
+
+def main(input):
+    if (input == "") or (not isinstance(input,str)):
+        return "Invalid inputs"
+    else:
+        repo = requests.get('https://api.github.com/users/' + input + '/repos')
+        list = json.loads(repo.text)
+    try:
+        for i in list:
+            name = i['name']
+            commits = requests.get('https://api.github.com/repos/' + input + '/' + name + '/commits')
+            commits_data = len(commits.json())
+        print('Repository Name:' + name + ' Number of commits: ' + str(commits_data))
+    except:
+        pass
+    return False
+
+
+
+
+
+print(main(input))
