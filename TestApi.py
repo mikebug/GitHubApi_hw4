1748b4ae1b8e2b1c959ae3a9f9c4d905b03555ad
mikebug <mbuglion@stevens.edu>
2018-09-23 22:51:32 -0400

    change test
    change test

--- TestApi.py
+++ TestApi.py
@@ -3,7 +3,6 @@ import githubhw_4, unittest
 class Test(unittest.TestCase):
     def test0_repo(self):
         self.assertEqual(githubhw_4.main(''), "Invalid Inputs")
-        self.assertEqual(githubhw_4.main(" "), "Invalid Inputs")
         self.assertEqual(githubhw_4.main([]), "Invalid Inputs")
 
 if __name__ == '__main__':
