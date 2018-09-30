5b10312614b9acacc6b3373e9062259bf61c5871
mikebug <mbuglion@stevens.edu>
2018-09-30 14:44:14 -0400

    added mock test
    added a mock test that removes the use of apis

--- TestApi.py
+++ TestApi.py
@@ -2,11 +2,21 @@
 import unittest
 
 from githubhw_4 import get_github_info
+from unittest import mock
 
 class Test(unittest.TestCase):
     def test0_repo(self):
         self.assertEqual(get_github_info(''), "Invalid Inputs")
         self.assertEqual(get_github_info([]), "Invalid Inputs")
+
+
+    @mock.patch('githubhw_4.get_github_info')
+    def test1_repositories(self, mock_get_repositories):
+        a = ['Repository Name:GitHubApi_hw4 Number of commits: 15', 'Repository Name:helloworld Number of commits: 3']
+        self.assertIn('Repository Name:GitHubApi_hw4 Number of commits: 15', a)
+        self.assertIn('Repository Name:helloworld Number of commits: 3', a)
+
+
 
 if __name__ == '__main__':
     unittest.main()
